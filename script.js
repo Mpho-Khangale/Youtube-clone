1960eb922fee86b27080c12e8fa78a5dccde2ee7
mpho <khangalempho16@gmail.com>
2026-06-08 01:22:39 +0200

    dark mode colour and code adjustments

--- script.js
+++ script.js
@@ -2,4 +2,11 @@ const darkModeBtn = document.querySelector(".dark-mode-btn");
 
 darkModeBtn.addEventListener("click", () => {
     document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        darkModeBtn.textContent = "☀";
+    } else {
+        darkModeBtn.textContent = "🌙";
+    }
+    
 });
